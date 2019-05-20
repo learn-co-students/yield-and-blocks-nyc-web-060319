b2e25da85b8d5b0c4403e0c3a3fe63575b4aae42
Alexander Riccio <test35965@gmail.com>
2019-05-20 16:53:59 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,12 @@
-def hello_t
-
+def hello_t(array)
+  if not block_given?
+    puts "Hey! No block was given!"
+    return
+  end
+  array.each do |elem|
+    yield elem
+  end
+  array
 end
 
 # call your method here!
